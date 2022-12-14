d9eb3d1c5cd2a35ff766a2f6f17f9f6766e4c9cd
Guzei <igor@guzei.com>
2022-12-14 11:39:12 +0300

    solved 2 problem: space between section in table

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -17,6 +17,8 @@ final class ProfileViewController: UIViewController {
         $0.delegate = self
         $0.register(PostTableViewCell.self, forCellReuseIdentifier: postIdentifier)
         $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.sectionFooterHeight = 0
+//        $0.sectionHeaderHeight = 0
         return $0
     }(UITableView(frame: .zero, style: .grouped)) // insetGrouped - красиво
 
@@ -37,21 +39,15 @@ final class ProfileViewController: UIViewController {
 extension ProfileViewController: UITableViewDataSource {
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        3
+        2
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        print("numberOfRowsInSection, section", section)
-        return section < 2 ? 1 : posts.count
+        section == 0 ? 1 : posts.count
     }
 
-//    func section
-
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        print("cellForRowAt, indexPath.section", indexPath.section, indexPath.row)
         if indexPath.section == 0 {
-            return PhotosTableViewCell()
-        } else if indexPath.section == 1 {
             return PhotosTableViewCell()
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: postIdentifier, for: indexPath) as? PostTableViewCell else { fatalError() }
@@ -64,31 +60,10 @@ extension ProfileViewController: UITableViewDataSource {
 extension ProfileViewController: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        print("viewForHeaderInSection", section)
-
-//        tableView.sectionFooterHeight = 100
-//        print(tableView.sectionFooterHeight)
-//        print(tableView.style.rawValue)
-//        print(tableView.separatorStyle)
-//        print(tableView.indicatorStyle)
-//        print(tableView.accessibilityNavigationStyle)
-//        print(tableView.overrideUserInterfaceStyle)
-
-        return section == 0 ? ProfileHeaderView() : nil
+        section == 0 ? ProfileHeaderView() : nil
     }
 
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-//        print("heightForHeaderInSection", section)
-        return section == 0 ? headerHeight : 0
-    }
-
-    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        print("heightForFooterInSection", section)
-        return 0
-    }
-
-    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        print("titleForFooterInSection", section)
-        return " " // если ничего не задать в футер, то не работает и задание его высоты. Возникает интервал между секциями.
+        section == 0 ? headerHeight : 0
     }
 }
